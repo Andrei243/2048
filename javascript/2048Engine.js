3ed6e56fb683165a23cbff5b121d7500182dd858
Mandel Andrei Nitu <andrei.nitu@essensys.ro>
2019-07-31 17:31:07 +0300

    Inceput parte de Javascript

--- javascript/2048Engine.js
+++ javascript/2048Engine.js
@@ -1,11 +1,13 @@
 window.addEventListener('load',()=>{
+let numerePosibile=[2];
+
 let reasezareValori = (vector)=>{
  for(let i=1;i<vector.length;i+=1){
-   if(vector[i].val!=0 && vector[i-1].val==0){
+   if(parseInt(vector[i].attr('val'))!=0 && parseInt(vector[i-1].attr('val'))===0){
       let j=i;
-      while(j>0&&vector[j].val!=0 && vector[j-1].val==0){
-        vector[j-1].val=vector[j].val;
-        vector[j].val=0;
+      while(j>0&&parseInt(vector[j].attr('val'))!==0 && parseInt(vector[j-1].attr('val'))===0){
+        vector[j-1].attr('val',parseInt(vector[j].attr('val')));
+        vector[j].attr('val',0);
       }
    }
  }
@@ -14,9 +16,11 @@ let reasezareValori = (vector)=>{
 
 let adunareValori = (vector)=>{
   for(let i=0;i<vector.length-1;i+=1){
-    if(vector[i].val==vector[i+1].val){
-      vector[i].val*=2;
-      vector[i+1].val=0;
+    if(parseInt(vector[i].attr('val'))==parseInt(vector[i+1].attr('val'))){
+      vector[i].attr('val',parseInt(vector[i].attr('val'))*2);
+      if(parseInt(vector[i].attr('val'))>numerePosibile[numerePosibile.length-1])
+        {numerePosibile.push(parseInt(vector[i].attr('val')));}
+      vector[i+1].attr('val',0);
     }
 
   }
@@ -27,31 +31,41 @@ let executarePas = (rand)=>{
   rand=reasezareValori(rand);
   rand=adunareValori(rand);
   rand=reasezareValori(rand);
-  for(let i=0 ;i<rand.length;i++){
-    rand[i].innerHTML='';
-    let imagine= document.createElement('img');
-    imagine.src = '../Numere/'+rand[i].val+'.png';
-    rand[i].appendChild(imagine);
-  }
-
 };
+
+
+let returneazaNumar = ()=>Math.floor(Math.random()*numerePosibile.length);
+
+
+
 
 let Engine2048 = {
 divs:null,
+adaugaNumarNou:()=>{
+let vectorGol=[];
+for(let i=0;i<divs.length;i++){
+  for(let j=0;j<divs.length;j++){
+    if(parseInt(divs[i][j].attr('val'))===0)vectorGol.push(divs[i][j]);
+  }
+}
+let element = vectorGol[Math.floor(Math.random()*vectorGol.length)];
+element.attr('val',returneazaNumar());
+console.log(element,element.attr('val'));
+},
+
 init:()=>{
   divs= [];
   for(let i=0;i<4;i++){
     let linie = [];
       for(let j=0;j<4;j++){
-        console.log(i,j);
         let div=$('#'+i+'-'+j);
-        console.log(div);
         div.text('');
         div.attr('val',0);
         linie.push(div);
       }
     divs.push(linie);
   }
+
 },
 calculeazaScor:()=>{
   let scor=0;
@@ -61,11 +75,24 @@ for(let i=0;i<divs.length;i++){
   }
 }
   document.getElementById('scor').innerText=scor.toString();
+},
+refacereImagini : ()=>{
+for(let i=0;i<divs.length;i++){
+  for(let j=0;j<divs[i].length;j++){
+    divs[i][j].html('');
+    divs[i][j].append('<img src="../Numere/'+parseInt(divs[i][j].attr('val'))+'.png" width=100px height=100px/>')
+  }
 }
 
+}
 
 }
 Engine2048.init();
 Engine2048.calculeazaScor();
+for(let i=0;i<6;i++){
+  Engine2048.adaugaNumarNou();
+}
+divs[0][0].attr('val',2);
+Engine2048.refacereImagini();
 
 })
