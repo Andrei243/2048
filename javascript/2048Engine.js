00b31bf32b59e3a43dd6f5e5e8fa531503d55e0e
Mandel Andrei Nitu <andrei.nitu@essensys.ro>
2019-08-01 09:22:51 +0300

    Jocul este terminat si merge bine, dar dupa ce se termina jocul apar erori in javascript dar jocul continua sa mearga

--- javascript/2048Engine.js
+++ javascript/2048Engine.js
@@ -3,11 +3,12 @@ let numerePosibile=[2];
 
 let reasezareValori = (vector)=>{
  for(let i=1;i<vector.length;i+=1){
-   if(parseInt(vector[i].attr('val'))!=0 && parseInt(vector[i-1].attr('val'))===0){
+   if(parseInt(vector[i].attr('val'))!==0 && parseInt(vector[i-1].attr('val'))===0){
       let j=i;
-      while(j>0&&parseInt(vector[j].attr('val'))!==0 && parseInt(vector[j-1].attr('val'))===0){
+      while(j>0&&/*parseInt(vector[j].attr('val'))!==0 && */parseInt(vector[j-1].attr('val'))===0){
         vector[j-1].attr('val',parseInt(vector[j].attr('val')));
         vector[j].attr('val',0);
+        j--;
       }
    }
  }
@@ -16,7 +17,7 @@ let reasezareValori = (vector)=>{
 
 let adunareValori = (vector)=>{
   for(let i=0;i<vector.length-1;i+=1){
-    if(parseInt(vector[i].attr('val'))==parseInt(vector[i+1].attr('val'))){
+    if(parseInt(vector[i].attr('val'))!==0&& parseInt(vector[i].attr('val'))===parseInt(vector[i+1].attr('val'))){
       vector[i].attr('val',parseInt(vector[i].attr('val'))*2);
       if(parseInt(vector[i].attr('val'))>numerePosibile[numerePosibile.length-1])
         {numerePosibile.push(parseInt(vector[i].attr('val')));}
@@ -33,6 +34,7 @@ let executarePas = (rand)=>{
   rand=reasezareValori(rand);
 };
 
+let valoareDiv = (div)=>parseInt(div.attr('val'));
 
 let returneazaNumar = ()=>numerePosibile[Math.floor(Math.random()*numerePosibile.length)];
 
@@ -41,6 +43,21 @@ let returneazaNumar = ()=>numerePosibile[Math.floor(Math.random()*numerePosibile
 
 let Engine2048 = {
 divs:null,
+terminat:false,
+terminare:()=>{
+  for(let i=0;i<divs.length;i++){
+    for(let j=0;j<divs.length;j++){
+      if(parseInt(divs[i][j].attr('val'))!==0)return false;
+    }
+  }
+
+  for(let i=1;i<divs.length;i++){
+    for(let j=1;j<divs[i].length;j++){
+      if(valoareDiv(divs[i][j])===valoareDiv(divs[i-1][j])||valoareDiv(divs[i][j])===valoareDiv(divs[i][j-1]))return false;
+    }
+  }
+return true;
+},
 adaugaNumarNou:()=>{
 let vectorGol=[];
 for(let i=0;i<divs.length;i++){
@@ -50,7 +67,6 @@ for(let i=0;i<divs.length;i++){
 }
 let element = vectorGol[Math.floor(Math.random()*vectorGol.length)];
 element.attr('val',returneazaNumar());
-console.log(element,element.attr('val'));
 },
 
 init:()=>{
@@ -84,6 +100,94 @@ for(let i=0;i<divs.length;i++){
   }
 }
 
+},
+pasSus:()=>{
+  if(Engine2048.terminat)return;
+  let vectori = [];
+  for(let i=0;i<4;i++){
+    let vector=[];
+    for(let j=0;j<4;j++){
+      vector.push(divs[j][i]);
+    }
+    vectori.push(vector);
+  }
+
+  for(let i=0;i<4;i++){
+    executarePas(vectori[i]);
+  }
+  Engine2048.terminat=Engine2048.terminare();
+
+  if(Engine2048.terminat){
+    return;
+  }
+  Engine2048.adaugaNumarNou();
+  Engine2048.calculeazaScor();
+},
+pasJos:()=>{
+  if(Engine2048.terminat)return;
+  let vectori = [];
+  for(let i=0;i<4;i++){
+    let vector=[];
+    for(let j=0;j<4;j++){
+      vector.push(divs[3-j][i]);
+    }
+    vectori.push(vector);
+  }
+
+  for(let i=0;i<4;i++){
+    executarePas(vectori[i]);
+  }
+  Engine2048.terminat=Engine2048.terminare();
+
+  if(Engine2048.terminat){
+    return;
+  }
+  Engine2048.adaugaNumarNou();
+  Engine2048.calculeazaScor();
+},
+pasStanga:()=>{
+  if(Engine2048.terminat)return;
+  let vectori = [];
+  for(let i=0;i<4;i++){
+    let vector=[];
+    for(let j=0;j<4;j++){
+      vector.push(divs[i][j]);
+    }
+    vectori.push(vector);
+  }
+
+  for(let i=0;i<4;i++){
+    executarePas(vectori[i]);
+  }
+  Engine2048.terminat=Engine2048.terminare();
+
+  if(Engine2048.terminat){
+    return;
+  }
+  Engine2048.adaugaNumarNou();
+  Engine2048.calculeazaScor();
+},
+pasDreapta:()=>{
+  if(Engine2048.terminat)return;
+  let vectori = [];
+  for(let i=0;i<4;i++){
+    let vector=[];
+    for(let j=0;j<4;j++){
+      vector.push(divs[i][3-j]);
+    }
+    vectori.push(vector);
+  }
+
+  for(let i=0;i<4;i++){
+    executarePas(vectori[i]);
+  }
+  Engine2048.terminat=Engine2048.terminare();
+
+  if(Engine2048.terminat){
+    return;
+  }
+  Engine2048.adaugaNumarNou();
+  Engine2048.calculeazaScor();
 }
 
 }
@@ -93,5 +197,25 @@ for(let i=0;i<6;i++){
   Engine2048.adaugaNumarNou();
 }
 Engine2048.refacereImagini();
+document.addEventListener('keydown',(e)=>{
+let tasta = e.keyCode;
+if(tasta === 38){
+  Engine2048.pasSus();
+  Engine2048.refacereImagini();
+}
+else if(tasta===40){
+  Engine2048.pasJos();
+  Engine2048.refacereImagini();
+}
+else if(tasta===37){
+  Engine2048.pasStanga();
+  Engine2048.refacereImagini();
+}
+else if(tasta===39){
+  Engine2048.pasDreapta();
+  Engine2048.refacereImagini();
+}
+
+})
 
 })
